60536f6f1eb0cd7045f8071c4714e70556a75644
Victor Varela <vitaotapioqueiro@gmail.com>
2021-07-11 01:42:15 -0300

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
+const headerNav = document.getElementById('header-nav');
+const btnUnmuted = document.getElementById('btnUnmuted');
+const btnMuted = document.getElementById('btnMuted');
+const vid = document.getElementById("bgvid");
 
+const modifyHeader = (event) => {
+    
+}
+
+// falta fazer função modifyHeader
+
+headerNav.addEventListener('scroll', modifyHeader);
+
+
+const enableMute = (event) => {
+    vid.muted = true;
+    btnMuted.classList.remove('btnNone');
+    btnUnmuted.classList.add('btnNone')
+}
+const disableMute = (event) => {
+    vid.muted = false;
+    btnUnmuted.classList.remove('btnNone');
+    btnMuted.classList.add('btnNone')
+}
+
+btnMuted.addEventListener('click', disableMute);
+btnUnmuted.addEventListener('click', enableMute);
