5d1024423351b167d8cea7157a1cbe2ce037b2d5
Victor Varela <vitaotapioqueiro@gmail.com>
2021-07-12 14:57:41 -0300

    commit de finalizacao, aguardar correcoes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ const btnMuted = document.getElementById('btnMuted');
 const vid = document.getElementById("bgvid");
 
 const modifyHeader = (event) => {
-    
+
 }
 
 // falta fazer função modifyHeader
@@ -15,13 +15,22 @@ headerNav.addEventListener('scroll', modifyHeader);
 const enableMute = (event) => {
     vid.muted = true;
     btnMuted.classList.remove('btnNone');
-    btnUnmuted.classList.add('btnNone')
+    btnUnmuted.classList.add('btnNone');
 }
 const disableMute = (event) => {
     vid.muted = false;
     btnUnmuted.classList.remove('btnNone');
-    btnMuted.classList.add('btnNone')
+    btnMuted.classList.add('btnNone');
 }
 
 btnMuted.addEventListener('click', disableMute);
 btnUnmuted.addEventListener('click', enableMute);
+
+
+let tamanhoTela = window.matchMedia("(min-width: 769px");
+
+if (tamanhoTela.matches === true){
+    vid.muted = true;
+    btnMuted.classList.remove('btnNone');
+    btnUnmuted.classList.add('btnNone');
+}
